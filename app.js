2f6a2e520d9fef776b3548e97b186fbbb9ffd93e
PioKl <kosiu342@gmail.com>
2023-10-12 16:34:03 +0200

    Using Env Variables

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    "mongodb+srv://kosiu342:Hgb00MTONxowWIeW@cluster0.firzuye.mongodb.net/mern?retryWrites=true&w=majority"
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.firzuye.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(5000);
