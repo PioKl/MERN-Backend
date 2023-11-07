ad11249ef07d2a793c26c1c00d07052bcbca0ae6
PioKl <kosiu342@gmail.com>
2023-11-07 15:37:16 +0100

    index changes

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ const HttpError = require("./models/http-error");
 
 const app = express();
 
+///
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+
 //middleware
 app.use(bodyParser.json());
 
@@ -56,6 +60,66 @@ app.use((error, req, res, next) => {
   }
   res.status(error.code || 500);
   res.json({ message: error.message || "An unknown error occurred!" });
+});
+
+// Endpoint do pobierania plików z S3 Bucket
+app.get("*", async (req, res) => {
+  let filename = req.path.slice(1);
+
+  try {
+    let s3File = await s3
+      .getObject({
+        Bucket: process.env.BUCKET,
+        Key: filename,
+      })
+      .promise();
+
+    res.set("Content-type", s3File.ContentType);
+    res.send(s3File.Body.toString()).end();
+  } catch (error) {
+    if (error.code === "NoSuchKey") {
+      console.log(`No such key ${filename}`);
+      res.sendStatus(404).end();
+    } else {
+      console.log(error);
+      res.sendStatus(500).end();
+    }
+  }
+});
+
+// Endpoint do zapisywania plików w S3 Bucket
+app.put("*", async (req, res) => {
+  let filename = req.path.slice(1);
+
+  await s3
+    .putObject({
+      Body: JSON.stringify(req.body),
+      Bucket: process.env.BUCKET,
+      Key: filename,
+    })
+    .promise();
+
+  res.set("Content-type", "text/plain");
+  res.send("ok").end();
+});
+
+// Endpoint do usuwania plików z S3 Bucket
+app.delete("*", async (req, res) => {
+  let filename = req.path.slice(1);
+
+  await s3
+    .deleteObject({
+      Bucket: process.env.BUCKET,
+      Key: filename,
+    })
+    .promise();
+
+  res.set("Content-type", "text/plain");
+  res.send("ok").end();
+});
+
+app.use("*", (req, res) => {
+  res.sendStatus(404).end();
 });
 
 mongoose
