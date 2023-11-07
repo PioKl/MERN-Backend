72846b8e5b5c7df573682a4865e8ff7197db4a83
PioKl <kosiu342@gmail.com>
2023-11-07 13:11:32 +0100

    corst changes

--- index.js
+++ index.js
@@ -27,6 +27,13 @@ app.use((req, res, next) => {
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://mern-frontend-a2805.web.app/"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
 
   next();
 });
