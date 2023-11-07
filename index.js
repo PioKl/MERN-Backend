c50c105b503cf917005e19d6f1cc856783b6b2ea
PioKl <kosiu342@gmail.com>
2023-11-07 13:00:39 +0100

    cors images change

--- index.js
+++ index.js
@@ -23,10 +23,6 @@ app.use("/uploads/images", express.static(path.join("uploads", "images")));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://nutty-jaguar.cyclic.app"
-  );
-  res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
