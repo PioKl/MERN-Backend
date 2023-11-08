3bc698eea106e7fe1cfb3cb3a736eed37a3c6909
PioKl <kosiu342@gmail.com>
2023-11-08 17:47:50 +0100

    Aws update

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -1,6 +1,7 @@
 const multer = require("multer");
 const AWS = require("aws-sdk");
 const uuid = require("uuid").v4;
+const fs = require("fs");
 
 const s3 = new AWS.S3();
 
@@ -12,9 +13,17 @@ const MIME_TYPE_MAP = {
 
 const fileUpload = multer({
   limits: 500000,
-  storage: multer.memoryStorage(), // `memoryStorage` zamiast `diskStorage` dla przesyłania do AWS S3
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "uploads/images"); // Lokalny katalog do przechowywania plików
+    },
+    filename: (req, file, cb) => {
+      const ext = MIME_TYPE_MAP[file.mimetype]; // Rozszerzenie pliku
+      cb(null, uuid() + "." + ext);
+    },
+  }),
 
-  //żeby nie dodać pliku o złym rozszerzeniu
+  // Sprawdzenie poprawności typu pliku
   fileFilter: (req, file, cb) => {
     const isValid = !!MIME_TYPE_MAP[file.mimetype]; //!! jeśli undefined/null to konwersja do false, bądź true, jeśli było false(0,null, undefined) to będzie false, w przeciwnym wypadku true
     const error = isValid ? null : new Error("Invalid mime type!");
@@ -35,16 +44,26 @@ const uploadToS3 = async (req, res, next) => {
         throw error;
       }
 
+      // Pobierz lokalną ścieżkę pliku
+      const localFilePath = req.file.path;
+
       const ext = MIME_TYPE_MAP[req.file.mimetype];
       const Key = uuid() + "." + ext;
 
       await s3
         .upload({
-          Body: req.file.buffer,
+          Body: fs.createReadStream(localFilePath),
           Bucket: process.env.CYCLIC_BUCKET_NAME,
           Key: Key,
         })
         .promise();
+
+      // Usuń plik lokalny po przesłaniu do AWS S3
+      fs.unlink(localFilePath, (err) => {
+        if (err) {
+          console.error("Failed to delete local file:", err);
+        }
+      });
 
       next();
     } catch (error) {
